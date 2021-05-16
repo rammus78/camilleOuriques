a9b5e252dc695728eb5765b94f438fd4b2819af1
CamilleOuriques <84253842+CamilleOuriques@users.noreply.github.com>
2021-05-16 13:54:57 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    float entrada=0, fixo=0, subtotal=0, taxa=0, total=0;
+    int start=0;
+    do{
+        //Inicio
+        printf("\nBem vindx a casa de cambio Muito Dinheiro, para dar inicio ao programa digite 1: ");
+        scanf("%i", &start);
+        //Valor pago em dinheiro
+        printf("\nDigite o valor de entrada: ");
+        scanf("%f", &entrada);
+        //Valor da moeda unitaria de destido (saida)
+        printf("\nDigite o valor atual (unitario) da moeda de destino: ");
+        scanf("%f", &fixo);
+
+        switch(start){
+            case 1:
+                //Valor bruto
+                subtotal = entrada * fixo;
+                //Valor da taxa
+                taxa = subtotal * 0.10;
+                //Valor final
+                total = subtotal + taxa;
+                default:
+                    printf("\nDigite 1 para recomecar\n");
+                    break;
+
+        }
+        printf("\nSubtotal= %0.2f", subtotal);
+        printf("\nFixo=", fixo);
+        printf("Taxa=%0.2f", taxa);
+        printf("Total= %0.2f", total);
+        printf("\nDigite 1 para recomecar\n");
+        scanf("%i", &start);
+
+    }while(start==1);
+}
